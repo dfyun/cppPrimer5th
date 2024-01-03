b12f2a2cc90f8256da6168be6bdbfcf424fa8167
fuyunding <dfy@fuyundingdeMacBook-Pro.local>
2024-01-03 22:57:08 +0800

    add .vscode and config task

---
+++ ch07/ex7.13.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ex7_12.h"
+
+int main()
+{
+    Sales_data total;
+    for (int i = 0; i < 10; ++i)
+        std::cout << i << " ";
+    std::cout << std::endl;
+    std::cout << "hello word!\n";
+    return 0;
+}
